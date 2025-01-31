f1ad57fe6f5789536fac6802084f046848e42c0d
contactkarthikrajendran <110722371+contactkarthikrajendran@users.noreply.github.com>
2025-01-31 08:27:54 +0530

    <Karthik> Navigation setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
-};
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
 };
 
 const App = () => {
